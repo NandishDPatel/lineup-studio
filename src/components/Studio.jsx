4f399e3ad0622131b3b3494f916eb73f875badab
unknown <nandishdpatel22@gmail.com>
2025-10-08 15:48:45 +0530

    fixing studio slider

--- src/components/Studio.jsx
+++ src/components/Studio.jsx
@@ -41,7 +41,7 @@ const Studio = forwardRef((props, ref) => {
         </div>
 
         <div
-          className="slideshow w-full h-auto sm:h-screen overflow-hidden bg-black"
+          className="slideshow w-full overflow-hidden bg-black"
           ref={ref}
         >
           <div
@@ -51,13 +51,13 @@ const Studio = forwardRef((props, ref) => {
             {studioPhotos.map((photo) => (
               <div
                 key={photo.id}
-                className="gallery-cell flex-shrink-0 w-full h-auto sm:h-screen flex items-center justify-center"
+                className="gallery-cell flex-shrink-0 w-full sm:h-screen flex items-center justify-center"
               >
                 <img
                   src={photo.url}
                   alt="studio image"
                   loading="lazy"
-                  className="object-cover"
+                  className="object-cover w-full sm:h-full"
                 />
               </div>
             ))}
