8ebdda70cf9a4108b9ba7e258334d7f4d813987f
unknown <nandishdpatel22@gmail.com>
2025-12-22 12:15:53 -0500

    uncommented the landing page code

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Header from './components/Header';
-import Content from './components/Content';
-import Footer from './components/Footer';
-import LandingPage from './components/LandingPage';
-import { AnimatePresence, motion } from 'framer-motion';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import LandingPage from "./components/LandingPage";
+import { AnimatePresence, motion } from "framer-motion";
 
 function App() {
   const [showLanding, setShowLanding] = useState(true);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setShowLanding(false);
-  //   }, 3000); 
-  //   return () => clearTimeout(timer);
-  // }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLanding(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    // <AnimatePresence mode="wait">
-    //   {showLanding ? (
-    //     <motion.div
-    //       key="landing"
-    //       initial={{ opacity: 1 }}
-    //       exit={{ opacity: 0 }}
-    //       transition={{ duration: 1 }} // fade-out duration
-    //     >
-    //       <LandingPage />
-    //     </motion.div>
-    //   ) : (
-    //     <motion.div
-    //       key="home"
-    //       initial={{ opacity: 0 }}
-    //       animate={{ opacity: 1 }}
-    //       transition={{ duration: 1 }} // fade-in duration
-    //     >
-    //       <Header />
-    //       <Content />
-    //       <Footer />
-    //     </motion.div>
-    //   )}
-    // </AnimatePresence>
-    <>
-      <Header />
-      <Content />
-      <Footer />
-    </>
+    <AnimatePresence mode="wait">
+      {showLanding ? (
+        <motion.div
+          key="landing"
+          initial={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1 }} // fade-out duration
+        >
+          <LandingPage />
+        </motion.div>
+      ) : (
+        <motion.div
+          key="home"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }} // fade-in duration
+        >
+          <Header />
+          <Content />
+          <Footer />
+        </motion.div>
+      )}
+    </AnimatePresence>
+    // <>
+    //   <Header />
+    //   <Content />
+    //   <Footer />
+    // </>
   );
 }
 
